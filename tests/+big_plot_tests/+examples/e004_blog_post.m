1b593d8b200da60f03b3aa14d363c1bb63e1f500
Jim Hokanson <jim.hokanson@gmail.com>
2018-02-03 22:29:04 -0500

    added figure 3

--- tests/+big_plot_tests/+examples/e004_blog_post.m
+++ tests/+big_plot_tests/+examples/e004_blog_post.m
@@ -1,6 +1,6 @@
 function e004_blog_post()
 
-%%
+%% Figure 1
 y = [1 2 3 4 5 6 7 8 9 8 7 6 7 6 5 4 3 2 4 8 9 7 4 5 6 7 7 7 7 7];
 subplot(1,2,1)
 plot(y,'Linewidth',2)
@@ -10,7 +10,7 @@ plot(y,'Linewidth',2)
 set(gca,'xlim',[-1e6 1e6],'FontSize',16)
 set(gcf,'position',[1,1,800,400]);
 
-%%
+%% Figure 2
 %Jim
 figure(1)
 s1 = big_plot_tests.examples.e001_interestingInput('type',0);
@@ -27,4 +27,15 @@ ax2 = copyobj(s2.ax(2),h_fig);
 ax1.Position = s1.ax(1).Position;
 ax2.Position = s2.ax(2).Position;
 set(gcf,'position',[1,1,800,400]);
+
+%% Figure 3
+y = [1 2 3 4 5 6 7 8 9 8 7 6 7 6 5 4 3 2 4 8 9 7 4 5 6 7 7 7 7 7];
+subplot(1,2,1)
+plot(y,'-o','Linewidth',2)
+set(gca,'FontSize',16)
+subplot(1,2,2)
+plot(y,'-o','Linewidth',2)
+set(gca,'xlim',[-1e6 1e6],'FontSize',16)
+set(gcf,'position',[1,1,800,400]);
+
 end
