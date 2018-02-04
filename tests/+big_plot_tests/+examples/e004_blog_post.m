7277c08bceed172caf326282a87e0c800c05370d
Jim Hokanson <jim.hokanson@gmail.com>
2018-02-03 22:05:51 -0500

    added 2nd figure

--- tests/+big_plot_tests/+examples/e004_blog_post.m
+++ tests/+big_plot_tests/+examples/e004_blog_post.m
@@ -10,6 +10,21 @@ plot(y,'Linewidth',2)
 set(gca,'xlim',[-1e6 1e6],'FontSize',16)
 set(gcf,'position',[1,1,800,400]);
 
+%%
+%Jim
+figure(1)
+s1 = big_plot_tests.examples.e001_interestingInput('type',0);
 
+%Matlab
+figure(2)
+s2 = big_plot_tests.examples.e001_interestingInput('type',2,'y',s1.y,'t',s1.t);
 
+h_fig = figure(3);
+clf;
+ax1 = copyobj(s1.ax(1),h_fig);
+ax2 = copyobj(s2.ax(2),h_fig);
+
+ax1.Position = s1.ax(1).Position;
+ax2.Position = s2.ax(2).Position;
+set(gcf,'position',[1,1,800,400]);
 end
