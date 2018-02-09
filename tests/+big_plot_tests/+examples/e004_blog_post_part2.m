712430d2858742f9b49c68fa07350e32763d363e
Jim Hokanson <jim.hokanson@gmail.com>
2018-02-08 19:15:26 -0500

    blog update

--- tests/+big_plot_tests/+examples/e004_blog_post_part2.m
+++ tests/+big_plot_tests/+examples/e004_blog_post_part2.m
@@ -42,6 +42,13 @@ for i = 1:n_flags
     end
 end
 
-keyboard
+%keyboard
+
+plot(r','LineWidth',3)
+set(gca,'FontSize',18,'xticklabels',types);
+legend(flags, 'Interpreter', 'none','Location','northwest')
+xtickangle(60)
+set(gcf,'Position',[1 1 600 600])
+
 
 end
