2b4c7df5e09a530692a6ece51b1a1c868415763e
Jim Hokanson <jim.hokanson@gmail.com>
2020-05-26 23:45:41 -0400

    Update raw_line_data_options.m

--- +big_plot/@raw_line_data_options/raw_line_data_options.m
+++ +big_plot/@raw_line_data_options/raw_line_data_options.m
@@ -2,12 +2,32 @@ classdef raw_line_data_options
     %
     %   Class:
     %   big_plot.raw_line_data_options
+    %
+    %   Options for processing big_plot.raw_line_data
+    %
+    %   See Also
+    %   --------
+    %   big_plot.raw_line_data
     
     properties
-        get_x_data = true;
-        xlim = [];
-        get_calibrated = true;
-        get_raw = false;
+        get_x_data = true
+        %Populates the 'x' property in raw_line_data. Note this might be an
+        %expensive memory operation which is why we have the option to
+        %disable it
+        
+        
+        xlim = []
+        %Range over which to retrieve the data.
+        
+        get_calibrated = true
+        %If a calibration is present, this will return the calibrated data
+        %when true. Calibration is generally only present (I think) with
+        %streaming data.
+        
+        get_raw = false
+        %Retrieves the uncalibrated data. In conjunction with
+        %get_calibrated this allows us to potentially get both raw and
+        %calibrated data.
     end
     
     methods
