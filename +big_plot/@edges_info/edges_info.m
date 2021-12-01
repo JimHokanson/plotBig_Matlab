0572ff2a5f5633771b88d81fbce97d196351a75a
Jim Hokanson <jim.hokanson@gmail.com>
2021-12-01 08:29:12 -0600

    Update edges_info.m
    Fixed bug that only manifested with multiple channels

--- +big_plot/@edges_info/edges_info.m
+++ +big_plot/@edges_info/edges_info.m
@@ -2,6 +2,23 @@ classdef edges_info < handle
     %
     %   Class:
     %   big_plot.edges_info
+    %
+    %   This class holds info on edge data for plotting. Ideally
+    %   when a zoom is made we could just plot only the visible data.
+    %   Unfortunately, if the axes is listening for a data change, it may
+    %   discover that the new data are not as expansive as previously
+    %   thought, and may zoom in further. 
+    %
+    %   The new code tries to always plot anchor points on the left and
+    %   right side that prevent zooming in or out. 
+    %
+    %   This however is really tricky if you include NaNs, as your anchor
+    %   point can no longer be the first and last point, as those may not
+    %   be valid.
+    %
+    %   This code tries to establish the first and last valid points
+    %   on data setup as this doesn't change, but is needed everytime
+    %   the zoom level changes.
     
     properties
         x_values
@@ -34,6 +51,7 @@ classdef edges_info < handle
             n_chans = size(y,2);
             
             if isinteger(y)
+                %Integers don't have NANs!!!!
                 x1 = big_plot.utils.indexToTime(x,1);
                 xend = big_plot.utils.indexToTime(x,n_samples);
 
@@ -54,6 +72,8 @@ classdef edges_info < handle
             %n_chans x 2 -> [left right]
             temp_I = NaN(n_chans,2);
             
+            %X could be an array, datetime, duration, or object
+            %so we have a helper function to intialize
             temp_x = big_plot.utils.getXInit(x,[n_chans,2]);
             temp_y = NaN(n_chans,2,'like',y);
             for i = 1:n_chans
@@ -76,19 +96,26 @@ classdef edges_info < handle
                 return
             end
             
+            %Note, we don't need to anchor each channel to its left
+            %or right most valid point, because any channel that 
+            %is furthest out will anchor the rest
+            %
+            %That's why below we look for the left and right most 
+            %valid points, and just grab all channels at that point, even
+            %though some channels may not be valid at that point.
             [~,I] = min(temp_x(:,1));
             obj.x_t1 = temp_x(I,1);
             obj.x_I1 = temp_I(I,1);
             obj.output_x_left = repmat(obj.x_t1,1,2);
-            obj.output_y_left = NaN(n_chans,2);
-            obj.output_y_left(I,1:2) = temp_y(I,1);
+            obj.output_y_left = NaN(2,n_chans);
+            obj.output_y_left(1:2,I) = temp_y(I,1);
             
             [~,I] = max(temp_x(:,2));
         	obj.x_tend = temp_x(I,2);
             obj.x_Iend = temp_I(I,2);
             obj.output_x_right = repmat(obj.x_tend,1,2);
-            obj.output_y_right = NaN(n_chans,2);
-            obj.output_y_right(I,1:2) = temp_y(I,2);
+            obj.output_y_right = NaN(2,n_chans);
+            obj.output_y_right(1:2,I) = temp_y(I,2);
             
             obj.x_values = temp_x;
             obj.y_values = temp_y;
